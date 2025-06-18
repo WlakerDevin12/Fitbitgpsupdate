0604cf2eb20f2fdc7e1bf1baf6cb75b850809805
WlakerDevin12 <walkerdevin176@gmail.com>
2025-06-18 15:22:15 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,1 +1,10 @@
+project = 'FitBit Gps Update'
+author = 'Your Name'
+release = '1.0'
 
+extensions = []
+templates_path = ['_templates']
+exclude_patterns = []
+
+html_theme = 'alabaster'
+html_static_path = ['_static']    
